bd1c30a7494eaac760e79cdb11c7e9e2fb4d71fe
Vineeth B V <71359072+vineethbv@users.noreply.github.com>
2020-09-29 19:53:38 +0530

    Update billCalculator.java

--- Extra Programs (Week - 3)/billCalculator.java
+++ Extra Programs (Week - 3)/billCalculator.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
-class Main{
+class billCalculator{
 
-	public static void billCalculator (String[]args){
+	public static void main (String[]args){
 
 		Scanner s = new Scanner (System.in);
 
