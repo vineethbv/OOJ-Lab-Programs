470494c9b77412848e165c0793b5ca9496e871e5
vinsdragonis <37930821+vinsdragonis@users.noreply.github.com>
2020-11-08 22:41:30 +0530

    Create CMDLineArgs.java

---
+++ WEEK - 6/CMDLineArgs.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    double d;
+    
+    System.out.println("No of command line arguemnets are : " + args.length);
+    
+    System.out.println(" command line arguemnets are : ");
+    
+    for (int i=0; i < args.length; i++) {
+      d=Double.parseDouble(args[i]);
+      System.out.println(d);
+    }
+  }
+}
