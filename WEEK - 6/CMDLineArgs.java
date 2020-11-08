00c6ac8bd6ff6c18127d509e62152e7d309eca5f
vinsdragonis <37930821+vinsdragonis@users.noreply.github.com>
2020-11-08 23:08:46 +0530

    Update CMDLineArgs.java

--- WEEK - 6/CMDLineArgs.java
+++ WEEK - 6/CMDLineArgs.java
@@ -4,7 +4,7 @@ class Main {
     
     System.out.println("No of command line arguemnets are : " + args.length);
     
-    System.out.println(" command line arguemnets are : ");
+    System.out.println("Command line arguemnets are : ");
     
     for (int i=0; i < args.length; i++) {
       d=Double.parseDouble(args[i]);
