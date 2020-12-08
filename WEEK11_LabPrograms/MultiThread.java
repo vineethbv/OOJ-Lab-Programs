c1a2b7634fc4b812d3150f40517ef8e7fcdf01f5
Vineeth B V <71359072+vineethbv@users.noreply.github.com>
2020-12-08 15:14:41 +0530

    Update MultiThread.java

--- WEEK11_LabPrograms/MultiThread.java
+++ WEEK11_LabPrograms/MultiThread.java
@@ -1,26 +1,26 @@
 class NewThread implements Runnable {
 	private String name;
-  private int interval;
+        private int interval;
 	private Thread t;
 
 	NewThread(String threadname, int interval) {
 		this.name = threadname;
-    this.interval = interval;
+                this.interval = interval;
 		t = new Thread(this, name);
 		t.start();
 	}
 
 	public void run() {
-    int i = 1, n = 5;
+                int i = 1, n = 5;
 		try {
 			while(i <= n) {
-        System.out.println("Thread: " + this.name);
-        i++;
-        Thread.sleep(this.interval);
-      }
-    }
-    catch (InterruptedException e) {
-		  System.out.println(name + "Interrupted");
+                                System.out.println("Thread: " + this.name);
+                                i++;
+                                Thread.sleep(this.interval);
+                        }
+                }
+                catch (InterruptedException e) {
+		        System.out.println(name + "Interrupted");
 		}
 	}
 }
